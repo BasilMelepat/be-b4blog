d24312d6db3fa30f5ac7849c1a6c1c7bc21dd15a
Basil M <98621163+BasilMelepat@users.noreply.github.com>
2025-01-04 00:46:15 +0530

    jwt update

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -2,39 +2,37 @@
 require('dotenv').config();
 const jwt = require("jsonwebtoken")
 
-const authenticateToken = (req, res, next) => {
-
-
-
-    const authHeader = req.headers['authorization'];
-
-
-    const token = authHeader && authHeader.split(' ')[1];
-
-
-    if (token === null) {
-        res.status(501).json({ msg: "cannot procced without a token!" })
+const authenticateToken = async (req, res, next) => {
+    try {
+      const header = req.headers.authorization;
+      if (!header) {
+        // No token provided
+        return res
+          .status(401)
+          .json({ message: "Unauthorized: No token provided" });
+      }
+      const token = header.split(" ")[1];
+      if (!token) {
+        // No token provided
+        return res
+          .status(401)
+          .json({ message: "Unauthorized: No token provided" });
+      }
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET_KEY);
+      if (!decoded) {
+        // Invalid token
+        return res.status(403).json({ message: "Forbidden: Invalid token" });
+      }
+      // Attach the decoded user information to the request
+      req.user = decoded;
+      next();
+    } catch (err) {
+      // Internal server error
+      return res.status(500).json({ message: "Internal server error" });
     }
-
-    jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, user) => {
-        
-        if (err) {
-            res.status(502).json({ msg: "Invalid token found" });
-        }
-        else {
-            req.user = user;
-            console.log("user verified")
-            next();
-
-        }
-
-    })
-
-
-
-
-
-
-}
+  };
 
 module.exports = authenticateToken
+
+
+
